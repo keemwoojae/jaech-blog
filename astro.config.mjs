ea95907d58aca7a8c4dd18f5ecfa1d19025b558e
Woojae Kim <keemwoojae@gmail.com>
2026-03-16 10:47:07 +0900

    Initial commit: Jaech's Blog

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://keemwoojae.github.io',
+	base: '/jaech-blog',
+	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: 'github-light',
+				dark: 'dracula',
+			},
+			wrap: true,
+		},
+	},
+});
