400f7000d868696594dfcd085bf5b001e82653b3
Woojae Kim <keemwoojae@gmail.com>
2026-03-16 11:05:18 +0900

    fix: add trailing slash to base URL to fix broken navigation links

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://keemwoojae.github.io',
-	base: '/jaech-blog',
+	base: '/jaech-blog/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
